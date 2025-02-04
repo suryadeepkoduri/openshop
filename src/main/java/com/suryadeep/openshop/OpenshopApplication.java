b896a18e45e06d7e8ada11282dec5d9834956559
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-02-04 11:39:30 +0530

    chore: git repository initialized

---
+++ src/main/java/com/suryadeep/openshop/OpenshopApplication.java
@@ -0,0 +1,13 @@
+package com.suryadeep.openshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshopApplication.class, args);
+    }
+
+}
