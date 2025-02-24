d6d6b3eb642e01026f8052fb627bad22464ce6d4
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-02-24 20:32:07 +0530

    created User class

---
+++ src/main/java/com/suryadeep/openshop/model/User.java
@@ -0,0 +1,10 @@
+package com.suryadeep.openshop.model;
+
+public class User {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+}
